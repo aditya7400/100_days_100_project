b38d02b4af583c0fde8deeeef7ec253633b5c6f0
aditya7400 <shuklaaditya0047@gmail.com>
2026-06-24 11:25:23 +0530

    Day 1: Hello World

--- Day_1/main.py
+++ Day_1/main.py
@@ -1,1 +1,4 @@
-print("Hello World")
+print("""Welcome to the Band Name Generator.""")
+city = input("Which city do you live in? ")
+pet_name = input("What's the name of your pet? ")
+print(f"Your band name is {city} {pet_name}")
