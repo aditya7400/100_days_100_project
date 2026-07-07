e626cb6bacb8215b6aedb7511b4d5a4ffd422828
aditya7400 <shuklaaditya0047@gmail.com>
2026-07-07 11:51:36 +0530

    Day 5: Quiz Game

--- Day_6/main.py
+++ Day_6/main.py
@@ -1,5 +1,4 @@
 import pyfiglet
-from time import sleep
 result = pyfiglet.figlet_format("Quiz Time", font="ansi_shadow")
 
 print(result)
@@ -8,11 +7,11 @@ from Day_6.quiz_brain import QuizBrain
 from data import question_data
 from question_model import Question
 question_bank = []
-for dictionary in question_data:
-    question_bank.append(Question(dictionary["text"],dictionary["answer"]))
-# for question in question_bank:
-#     print(question.text,end=" : ")
-#     print(question.answer)
+for dictionary in question_data["results"]:
+    question = dictionary["question"]
+    answer = dictionary["correct_answer"]
+    question_bank.append(Question(text=question,answer=answer))
+
 quiz = QuizBrain(question_bank)
 while quiz.any_questions_left():
     quiz.next_question()
