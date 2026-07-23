447a934f264a1c35f7225311a8501e4ef33e98f0
aditya7400 <shuklaaditya0047@gmail.com>
2026-07-23 12:38:39 +0530

    State Game

--- Day_13/data_process.py
+++ Day_13/data_process.py
@@ -6,8 +6,8 @@ def give_cor(state_name):
     if state_name in data["state"].values:
 
         my_data = data[data["state"] ==state_name]
-        x_cor = my_data["x"].values[0]
-        y_cor = my_data["y"].values[0]
+        x_cor = my_data["x"].item()
+        y_cor = my_data["y"].item()
         return (int(x_cor),int(y_cor))
     else:
         print("not found")
--- Day_13/main.py
+++ Day_13/main.py
@@ -1,6 +1,7 @@
 import turtle
 from write import Write
-from data_process import give_cor
+import  pandas
+from data_process import give_cor , data
 pen = Write()
 screen = turtle.Screen()
 screen.title("India States Game")
@@ -15,20 +16,32 @@ game_is_on = True
 def get_mouse_click_coor(x,y):
    print(x , y)
 turtle.onscreenclick(get_mouse_click_coor)
+guessed_correctly  = []
 while game_is_on:
     state_name = take_input()
 
     cor = give_cor(state_name)
+    if state_name == "Exit":
+
+        to_learn_states = []
+        for state in data.state.to_list():
+            if state not in guessed_correctly:
+                to_learn_states.append(state)
+        game_data = pandas.DataFrame(to_learn_states,columns=["state"])
+        game_data.to_csv("Missed States")
+        break
+
+
     if cor:
         pen.guessed_out_of_30+=1
         pen.state_name(cor[0],cor[1],state_name)
+        guessed_correctly.append(state_name)
     if pen.guessed_out_of_30 == 30:
         game_is_on = False
         pen.goto(0,0)
         pen.write("You Won",align="center",font=("Courier", 70, "normal"))
 
 
-turtle.mainloop()
 
 
 
